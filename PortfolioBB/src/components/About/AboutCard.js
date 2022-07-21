f04a88c7346c1c12811d4b25dc57a49478c18a5a
BRYAN BLAIR <bryan.clawd.blair@gmail.com>
2022-07-21 14:47:43 +0200

    first commit

---
+++ PortfolioBB/src/components/About/AboutCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Bryan Blair </span>
+            from <span className="purple"> Johannesburg, South Africa.</span>
+            <br />I am a junior fullstack developer seeking employment based in Europe.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Geopolitics
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> History
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Adventure Travel
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Soumyajit</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
